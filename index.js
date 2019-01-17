1da9a92c638ac67edb7068d28edd0782aa9d40a1
BoredLama <43473329+BoredLama@users.noreply.github.com>
2019-01-17 16:44:56 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,91 @@
+const Parser = require('rss-parser')
+const parser = new Parser()
+
+const needle = require('needle')
+const cheerio = require('cheerio')
+const async = require('async')
+
+let top10 = []
+
+let oldImdbIds = []
+
+const getMeta = (imdbId, cb) => {
+    needle.get('https://v3-cinemeta.strem.io/meta/movie/' + imdbId + '.json', (err, resp, body) => {
+        cb(body && body.meta ? body.meta : false)
+    })
+}
+ 
+const populate = async () => {
+ 
+  let feed = await parser.parseURL('https://torrentfreak.com/category/dvdrip/feed/')
+ 
+  const imdbIds = []
+
+  feed.items.some(item => {
+    if (item.title.startsWith('Top 10 Most Pirated Movies of The Week')) {
+      if (item['content:encoded']) {
+      	const $ = cheerio.load(item['content:encoded'])
+        $('a').each((ij, el) => {
+          const href = $(el).attr('href');
+          if (href.startsWith('https://www.imdb.com/title/')) {
+            const imdbId = href.replace('https://www.imdb.com/title/', '').replace('/','')
+            if (imdbIds.indexOf(imdbId) == -1)
+              imdbIds.push(imdbId)
+          }
+        })
+        return true
+      }
+    }
+  })
+
+  if (!imdbIds.length || JSON.stringify(imdbIds) == JSON.stringify(oldImdbIds)) {
+  	return
+  }
+
+  oldImdbIds = imdbIds
+
+  const metas = []
+
+  const metaQueue = async.queue((imdbId, cb) => {
+  	getMeta(imdbId, (metaObj) => {
+  		if (metaObj)
+  			metas.push(metaObj)
+  		cb()
+  	})
+  }, 1)
+
+  metaQueue.drain = () => { top10 = metas }
+
+  imdbIds.forEach(imdbId => { metaQueue.push(imdbId) })
+
+}
+
+populate()
+
+setInterval(populate, 172800000) // populate every 2 days
+
+const addonSDK = require('stremio-addon-sdk')
+
+const addon = new addonSDK({
+    id: 'org.tftop10',
+    version: '0.0.1',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/TorrentFreak_logo.svg/982px-TorrentFreak_logo.svg.png',
+    name: 'TorrentFreak Top 10 Movies',
+    description: 'Add-on to show a Catalog for TorrenFreak\'s Weekly Top 10 Movies',
+    resources: ['catalog'],
+    types: ['movie'],
+    idPrefixes: ['tt'],
+    catalogs: [
+    	{
+    		type: 'movie',
+    		id: 'tktop10movies',
+    		name: 'Top 10 Most Pirated by TorrentFreak'
+    	}
+    ]
+})
+
+addon.defineCatalogHandler((args, cb) => {
+    cb(null, args.type === 'movie' && args.id === 'tktop10movies' ? { metas: top10 } : null)
+})
+
+addon.runHTTPWithOptions({ port: 7000 })
